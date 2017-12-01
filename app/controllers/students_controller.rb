435efe2a1f76a6c7d46756cbaf622410615e3f4e
Lisa Lee <snickers495@gmail.com>
2017-12-01 22:50:04 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create(first_name: params[:students][:first_name])
+    Student.create(first_name: params[:students][:first_name], last_name: params[:students][:last_name])
   end
 
 end
