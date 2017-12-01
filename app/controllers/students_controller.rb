8e80f5506cc7bba28f5afc6e468ae6b6f125e6f2
Lisa Lee <snickers495@gmail.com>
2017-12-01 22:48:13 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,11 +8,10 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new 
+  def new
+  end
 
-  end 
-
-  def create 
+  def create
 
   end
 
